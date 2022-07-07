f59b053f4bfeee979770c0edba1c2cdef9571ae5
Nataorjuela <nataorjuela62@gmail.com>
2022-07-07 15:56:27 -0500

    tablero multiusuario en tiempo real

---
+++ README.md
@@ -0,0 +1,30 @@
+# Interactive Board Real Time
+Crear un tablero en tiempo real usando web socket
+
+
+## Despliegue Heroku 
+
+
+[![Heroku]]()
+
+
+## Ejecutando
+
+Se abre dos pestañas de heroku. Se dibujará y se verá en real time 
+
+
+
+
+## Autor 
+
+
+* **Natalia Orjuela** 
+
+
+
+
+## Licencia 📌
+
+
+
+
---
+++ src/main/java/escuelaing/arsw/interactiveblackboard/boardrealtime/BoardRealTimeApplication.java
@@ -0,0 +1,13 @@
+package escuelaing.arsw.interactiveblackboard.boardrealtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardRealTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardRealTimeApplication.class, args);
+    }
+
+}
